6e192fc992a58921ca039ef9a281338f53d1cd36
pedroaraujo7 <pedro.araujop17@gmail.com>
2026-02-16 13:14:57 -0300

    estrutura html montada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { Container, Title } from './styles'
+import { Button, Container, ContainerInputs, Form, Input, InputLabel, Title, TopBackground } from './styles'
 
 
 function App() {
 
   return (
     <Container>
-      <Title>React</Title>
+      <TopBackground>
+        <img />
+      </TopBackground>
+
+
+      <Form>
+        <Title>Cadastrar Usuários</Title>
+        
+        <ContainerInputs>
+          <div>
+            <InputLabel>Nome <span>*</span></InputLabel>
+            <Input type="text" placeholder='Nome do usuário' />
+          </div>
+          
+          <div>
+            <InputLabel>Idade <span>*</span></InputLabel>
+            <Input type="number" placeholder='Idade do usuário' />
+          </div>
+        </ContainerInputs>
+
+          <div>
+            <InputLabel>E-mail <span>*</span></InputLabel>
+            <Input type='email' placeholder='E-mail do usuário' />
+          </div>
+
+        <Button>Cadastrar Usuário</Button>
+
+      </Form>
     </Container>
   )
 }
